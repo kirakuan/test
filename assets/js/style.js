ce37ea2227fc139d729c204d31b717ded3269583
nakabayashi kotaro <123windam@gmail.com>
2023-09-15 21:02:40 +0900

    リファクタ

--- assets/js/style.js
+++ assets/js/style.js
@@ -115,9 +115,11 @@ const append_contact = function () {
 
 const prepend_preparing = function () {
     $(".m-Article").prepend(`
-    <section class="container">
-        <h2 class="a-Title is_h2">サイト準備中です。</h2>
-        <p><a href="#c-Contact">連絡先はこちら！</a></p>
+    <section>
+        <div class="container c-Section">
+            <h2 class="a-Title is_h2">サイト準備中です。</h2>
+            <p><a href="#c-Contact">連絡先はこちら！</a></p>
+        </div>
     </section>
     `)
 }
