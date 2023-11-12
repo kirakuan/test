77b511c1563fbeadbe223c16b3f3807c2500c2fe
nakabayashi kotaro <123windam@gmail.com>
2023-11-12 16:21:58 +0900

    analytics 導入

--- app/__components/head.php
+++ app/__components/head.php
@@ -6,6 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $page_meta_info->title(); ?></title>
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-BB59ZG8HR5"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-BB59ZG8HR5');
+    </script>
+
     <!-- style -->
     <link href="/assets/css/bootstrap.css" rel="stylesheet">
     <link href="/assets/css/reset.css" rel="stylesheet">
@@ -17,4 +27,6 @@
     <!-- script -->
     <script src="/assets/js/jquery.min.js"></script>
     <script src="/assets/js/style.js"></script>
+
+
 </head>
